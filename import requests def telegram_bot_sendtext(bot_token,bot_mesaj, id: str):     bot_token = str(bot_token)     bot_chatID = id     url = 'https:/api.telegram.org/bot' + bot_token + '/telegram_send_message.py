ef9aa04d5a0266f215ee002b4da3fad3384dd17d
Only Fibonacci <101716916+OnlyFibonacci@users.noreply.github.com>
2022-04-09 16:04:03 +0300

    Delete import requests def telegram_bot_sendtext(bot_token,bot_mesaj, id: str):     bot_token = str(bot_token)     bot_chatID = id     url = 'https:/api.telegram.org/bot' + bot_token + ' directory

--- import requests def telegram_bot_sendtext(bot_token,bot_mesaj, id: str):     bot_token = str(bot_token)     bot_chatID = id     url = 'https:/api.telegram.org/bot' + bot_token + '/telegram_send_message.py
+++
@@ -1,8 +0,0 @@
-import requests
-def telegram_bot_sendtext(bot_token,bot_mesaj, id: str):
-    bot_token = str(bot_token)
-    bot_chatID = id
-    url = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + str(
-        bot_chatID) + '&parse_mode=Markdown&text=' + bot_mesaj
-
-    response = requests.get(url)
