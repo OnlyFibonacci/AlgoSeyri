8891a40fc701511c8cf00f7dd97f97c2cd632472
Only Fibonacci <101716916+OnlyFibonacci@users.noreply.github.com>
2022-04-18 00:41:04 +0300

    cross_above_added

--- indicators/function.py
+++ indicators/function.py
@@ -20,3 +20,17 @@ def lowest(series_a, length):
             newList.append(series_a[len(series_a) - 1 - x])
         return min(newList)
 
+def cross_above(series_a, series_b):
+    series_a = pd.Series(series_a) if isinstance(series_a, list) else series_a
+    series_b = pd.Series(series_b) if isinstance(series_b, list) else series_b if isinstance(series_b,pd.Series) else pd.Series(data=[series_b,series_b])
+    pre_short = series_a.iloc[-2]
+    short = series_a.iloc[-1]
+
+    pre_long = series_b.iloc[-2]
+    long = series_b.iloc[-1]
+
+
+    if pre_short < pre_long and short > long:
+        return True
+    else:
+        return False
