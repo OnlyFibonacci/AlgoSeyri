96a42953b80b6243018b58b665826a9800341214
Only Fibonacci <101716916+OnlyFibonacci@users.noreply.github.com>
2022-04-18 00:46:13 +0300

    cross above update

--- indicators/function.py
+++ indicators/function.py
@@ -21,6 +21,12 @@ def lowest(series_a, length):
         return min(newList)
 
 def cross_above(series_a, series_b):
+    """
+    Checks the up-intersection of the first series sent to the second series or value
+    :param series_a:
+    :param series_b:
+    :return: true or false
+    """
     series_a = pd.Series(series_a) if isinstance(series_a, list) else series_a
     series_b = pd.Series(series_b) if isinstance(series_b, list) else series_b if isinstance(series_b,pd.Series) else pd.Series(data=[series_b,series_b])
     pre_short = series_a.iloc[-2]
